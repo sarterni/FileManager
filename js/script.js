01f8284c454fe07e1ffdc8721e2a858fa4ce901b
sarterni <sarter.nicolas@gmail.com>
2024-04-11 10:12:48 +0200

    js

--- js/script.js
+++ js/script.js
@@ -12,4 +12,31 @@ addButton.addEventListener('click', function () {
 
 
     newButton.classList.add('grid-item');
+    newButton.id = 'btn-' + (buttons.length + 1);
 });
+
+
+const renameButton = document.querySelector('#rename');
+
+renameButton.addEventListener('click', function () {
+    const buttons = document.querySelectorAll('.grid-item');
+    const buttonId = prompt('Enter the id of the button you want to rename: (btn-1, btn-2, etc.)');
+    const buttonToRename = document.getElementById(buttonId);
+    if (buttonToRename) {
+        const newName = prompt('Enter the new name for the button:');
+        buttonToRename.textContent = newName;
+    } else {
+        console.log('Button not found!');
+    }
+});
+
+
+        const removeButton = document.querySelector('#remove');
+        
+        removeButton.addEventListener('click', function () {
+            const buttons = document.querySelectorAll('.grid-item');
+            if (buttons.length > 0) {
+                const lastButton = buttons[buttons.length - 1];
+                lastButton.remove();
+            }
+        });
